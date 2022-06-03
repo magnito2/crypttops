945fb9ca08581e75338030a40f027fc291e1f05c
magnito2 <magnusotwani@gmail.com>
2022-06-03 08:48:17 +0300

    Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,45 @@
+<script>
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { CryptSample } from '@/entry.esm';
+import { LeaderBoard } from '../src/lib-components';
+
+export default defineComponent({
+    name: "ServeDev",
+    data() {
+      return {
+        leaders: [
+        {
+          name: 'John Mitchel D.',
+          pairs: [1.234, 3.245, 2.650, -0.34743]
+        },
+        {
+          name: 'Liam',
+          pairs: [-1.234, 3.245, -2.650, 0.34743]
+        },
+        {
+          name: 'Tony Starks',
+          pairs: [10.234, 30.245, -15.650, 34.743]
+        },
+      ]
+      }
+    }
+});
+</script>
+
+<template>
+  <div id="app">
+    <LeaderBoard
+      :pairs="['ETHUSD','BTCUSD', 'XRPUSD', 'EOSUSD']"
+      :data="leaders"
+    />
+  </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap');
+body {
+    background: #1d1d1d;
+    font-family: 'Poppins', sans-serif;
+  }
+</style>
---
+++ src/lib-components/LeaderBoard.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="container-board col-7">
+    <div class="title-board col-3">
+      <img src="../assets/Chart.svg">
+      <h3>Leaderboard</h3>
+    </div>
+    <div class="table">
+        <table>
+            <thead>
+                <tr>
+                    <th>Rank</th>
+                    <th>Username</th>
+                    <th v-for="(pair, idx) in pairs" :key="idx">{{ pair }}</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(userData, idx) in data" :key="idx">
+                    <td>{{ idx+1 }}</td>
+                    <td>{{ userData.name }}</td>
+                    <td v-for="(pair, idx) in userData.pairs" :key="idx">
+                        <span v-if="pair > 0" class="pos">{{ pair }}%</span>
+                        <span v-else class="neg">{{ pair }}%</span>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'LeaderBoard',
+    props: {'pairs': Array, 'data': Array},
+    components: {
+
+    },
+    setup() {
+        
+    },
+
+}
+</script>
+
+<style lang="scss" scoped>
+@media (min-width: 1024px) {
+  .col-1 {
+    width: 8.33%;
+  }
+  .col-2 {
+    width: 16.66%;
+  }
+  .col-3 {
+    width: 25%;
+  }
+  .col-4 {
+    width: 33.33%;
+  }
+  .col-5 {
+    width: 41.66%;
+  }
+  .col-6 {
+    width: 50%;
+  }
+  .col-7 {
+    width: 58.33%;
+  }
+  .col-8 {
+    width: 66.66%;
+  }
+  .col-9 {
+    width: 75%;
+  }
+  .col-10 {
+    width: 83.33%;
+  }
+  .col-11 {
+    width: 91.66%;
+  }
+  .col-12 {
+    width: 100%;
+  }
+
+  .container-board {
+    background: #070707;
+    display: flex;
+    flex-direction: column;
+    border-radius: 10px;
+    color: #9f9f9f;
+    margin: 10px 32px;
+  }
+  .title-board {
+    margin: auto 14px;
+    display: flex;
+  }
+  .title-board img {
+    margin: auto 14px;
+    max-height: 25px;
+  }
+
+  .pos {
+    color: lime;
+  }
+  .neg {
+    color: red;
+  }
+
+  .table {
+      margin: auto 20px;
+      width: 100%;
+      table {
+          width: 100%;
+          th, td {
+              text-align: left;
+              font-size: 12px;
+              padding: 5px;
+          }
+      }
+  }
+}
+</style>
